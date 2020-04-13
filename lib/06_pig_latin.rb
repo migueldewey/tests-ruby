0b8b14b6ad3e2011bf098ee493075d83fba591f0
migueldewey <62953034+migueldewey@users.noreply.github.com>
2020-04-13 23:27:57 +0200

    Add files via upload

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,7 +1,19 @@
 def translate(word)
+	case word
+	when word.include? " "
+		word = word.split
+	end
+	if word.include? " "
+		word = word.split
+	end
 	if word[0]== 'a' || word[0]== 'e' || word[0]== 'i' || word[0]== 'o' ||word[0]== 'u'
 		return word + "ay"
-	else 
+	elsif word[1]== 'a' || word[1]== 'e' || word[1]== 'i' || word[1]== 'o' ||word[1]== 'u'
 		return word.reverse + "ay"
+	else 
+		word = word + word[0] + word[1] + "ay"
+		word[0] = ""
+		word[0] = ""
+		return word
 	end
 end
